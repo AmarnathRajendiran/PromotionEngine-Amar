9065fec8bc6d8605c5fb8ee33aa6347c0d235b9e
Amarnath R <amarnath.rajendiran@globallogic.com>
2020-07-02 15:48:37 +0530

    Add project files.

---
+++ PromotionEngine-Amar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PromotionEngine_Amar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
